8ca532688f223e861a0de5ddd164f2b7572ee128
MeganMoore738 <meganmoore738@gmail.com>
2016-11-23 10:11:40 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-// menu toggle button
-function myFunction(x) {
-    x.classList.toggle("change");
-}
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 
  // page scroll
